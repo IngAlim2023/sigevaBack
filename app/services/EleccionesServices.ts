c6589ca1f185508297ed653539ee0653fab0f673
andres naranjo <santo2828@gmail.com>
2025-09-15 22:06:16 -0500

    implementacion listarElecccionesPorCentroF

--- app/services/EleccionesServices.ts
+++ app/services/EleccionesServices.ts
@@ -4,6 +4,20 @@
 import Eleccione from "#models/eleccione"
 
 export default class EleccionService {
+  async listarPorCentro(idcentro_formacion: any) {
+      const elecciones = await Eleccione.query()
+      .where('idcentro_formacion', idcentro_formacion)
+      .preload('centro')
+      .preload('candidato', (candidatoQuery) => {
+        candidatoQuery.preload('aprendiz', (aprendizQuery) => {
+          aprendizQuery
+            .preload('grupo')
+            .preload('programa')
+        })
+      })
+
+    return elecciones
+  }
   
   async eleccionesAactivas() {
     const today = new Date()
@@ -11,4 +25,6 @@ export default class EleccionService {
     return await Eleccione.query()
     .where('fecha_fin', '>=', today)
   }
+
+
 }
